0ec3b9922ce0f9f2ae241596c206aa88106cff99
Poucy113 <poucy.cy113@gmail.com>
2021-02-18 13:13:51 +0100

    mé y a pa d'ereur

--- src/org/lcdd/ses/back/GraphUpdater.java
+++ src/org/lcdd/ses/back/GraphUpdater.java
@@ -1,7 +1,5 @@
 package org.lcdd.ses.back;
 
-import java.lang.*;
-
 public class GraphUpdater{
 
     private static int buyPrice = 100;
@@ -16,18 +14,18 @@ public class GraphUpdater{
     Thread th = new Thread(new Runnable() {
         @Override
         public void run() {
-            int max = buyPrice + 10;
-            int min = buyPrice - 10;
-            int newPrice = (int) ((max-min)*Math.random()) + min;
-            buyPrice = newPrice;
-            System.out.println(buyPrice);
-            int maxSell = sellPrice + 10;
-            int minSell = sellPrice - 10;
-            int newPriceSell = (int) ((maxSell-minSell)*Math.random()) + minSell;
-            sellPrice = newPriceSell;
-            System.out.println(sellPrice);
             try {
-                th.sleep(1000);
+            	int max = buyPrice + 10;
+                int min = buyPrice - 10;
+                int newPrice = (int) ((max-min)*Math.random()) + min;
+                buyPrice = newPrice;
+                System.out.println(buyPrice);
+                int maxSell = sellPrice + 10;
+                int minSell = sellPrice - 10;
+                int newPriceSell = (int) ((maxSell-minSell)*Math.random()) + minSell;
+                sellPrice = newPriceSell;
+                System.out.println(sellPrice);
+				Thread.sleep(1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
