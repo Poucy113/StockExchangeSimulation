9d98e1e4dbaa3f71cdf7afafd40c119efb9d5ac7
Poucy113 <poucy.cy113@gmail.com>
2021-02-18 13:15:24 +0100

    graph updater

--- src/org/lcdd/ses/back/GraphUpdater.java
+++ src/org/lcdd/ses/back/GraphUpdater.java
@@ -4,33 +4,39 @@ public class GraphUpdater{
 
     private static int buyPrice = 100;
     private static int sellPrice = 90;
+    
+    private static Thread th;
+    
+    public GraphUpdater() {
+		this.th = new Thread(new Runnable() {
+	        @Override
+	        public void run() {
+	            try {
+	            	int max = buyPrice + 10;
+	                int min = buyPrice - 10;
+	                int newPrice = (int) ((max-min)*Math.random()) + min;
+	                buyPrice = newPrice;
+	                System.out.println(buyPrice);
+	                int maxSell = sellPrice + 10;
+	                int minSell = sellPrice - 10;
+	                int newPriceSell = (int) ((maxSell-minSell)*Math.random()) + minSell;
+	                sellPrice = newPriceSell;
+	                System.out.println(sellPrice);
+					Thread.sleep(1000);
+	            } catch (InterruptedException e) {
+	                e.printStackTrace();
+	            }
+	        }
+	    });
+		th.setName("GraphUpdater");
+		th.start();
+	}
 
     public static int getBuyPrice() {
         return buyPrice;
     }
     public static int getSellPrice() {return sellPrice;}
 
-
-    Thread th = new Thread(new Runnable() {
-        @Override
-        public void run() {
-            try {
-            	int max = buyPrice + 10;
-                int min = buyPrice - 10;
-                int newPrice = (int) ((max-min)*Math.random()) + min;
-                buyPrice = newPrice;
-                System.out.println(buyPrice);
-                int maxSell = sellPrice + 10;
-                int minSell = sellPrice - 10;
-                int newPriceSell = (int) ((maxSell-minSell)*Math.random()) + minSell;
-                sellPrice = newPriceSell;
-                System.out.println(sellPrice);
-				Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-    });
-
+    public static Thread getUpdaterThread() {return th;}
 
 }
